81009595af9a7968a3e8b19c159683272c1e3581
Ivan <belogurov.ivan@list.ru>
2022-12-10 10:45:18 +0500

    improved the model's accurancy (~87%) and reduced the model's loss (~1.24)

--- create_nn.py
+++ create_nn.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from tensorflow import keras
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 
 df_train = pd.read_csv('train_dataset.csv')
 
@@ -11,15 +11,30 @@ x_train = x_train / 640
 
 y_train_cat = keras.utils.to_categorical(y_train, 8)
 
-model = keras.models.Sequential([Dense(256, input_shape=(42,), activation='relu'),
-                                Dense(524, activation='relu'),
-                                Dense(1048, activation='relu'),
+model = keras.models.Sequential([Dense(32, input_shape=(42,), activation='relu'),
+                                Dense(54, activation='relu'),
+                                Dense(86, activation='relu'),
+                                Dense(156, activation='relu'),
+                                Dropout(rate=0.8),
                                 Dense(8, activation='softmax')])
 
 model.compile(optimizer='adam',
              loss='categorical_crossentropy',
              metrics=['accuracy'])
 
-model.fit(x_train, y_train_cat, batch_size=32, epochs=10, validation_split=0.3)
+model.fit(x_train, y_train_cat, batch_size=400, epochs=12, validation_split=0.2)
+
+df_test = pd.read_csv('train_dataset.csv')
+
+x_test = df_test.drop(['y'], axis=1)
+y_test = df_test['y']
+
+x_test = x_test / 640
+
+y_test_cat = keras.utils.to_categorical(y_test, 8)
+
+result = model.evaluate(x_test, y_test_cat)
+
+print(result)
 
 model.save("gestures_model.h5")
